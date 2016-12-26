f0eb9f29f0d4bfa3df46069567ed9ee42574950c
Jason Harris <jasonh@wolfram.com>
2016-12-26 11:19:58 +0200

    - release version 0.7.5

--- banksync/banksync.py
+++ banksync/banksync.py
@@ -13,7 +13,7 @@ from collections import OrderedDict
 from sysexecute import *
 from banksync_common import *
 
-__version__ = "0.7.3"
+__version__ = "0.7.5"
 
 
 # --------------------------------------------------------------------------------------------------------------------------
