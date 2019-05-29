06aedbf02cc9677b0ae1e4d5184fcf533f20d0f0
Jason Harris <jasonh@wolfram.com>
2019-05-29 05:57:33 +0200

    - Change release scripts.

--- tools/releasePyPiProduction.sh
+++ tools/releasePyPiProduction.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-pip uninstall banksync
-python setup.py sdist upload -r pypi
-sleep 3
-pip install banksync
+# https://packaging.python.org/tutorials/packaging-projects/
+
+# cd to /Development/Python/sysexecute_Package
+# it is important the script is run from there.
+
+pip3 uninstall banksync
+python3 setup.py sdist bdist_wheel
+
+#python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# Enter the TestPyPI user: jasonfh
+# Enter the TestPyPI pass: **************
+
+# The following uses the information in ~/.pypirc
+python3 -m twine upload --repository pypi dist/*
+
+
+sleep 5
+
+# install the package locally from TestPyPI:
+pip3 install banksync
--- tools/releasePyPiTest.sh
+++ tools/releasePyPiTest.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-pip uninstall banksync
-python setup.py sdist upload -r pypitest
-sleep 3
-pip install -i https://testpypi.python.org/pypi banksync
+# https://packaging.python.org/tutorials/packaging-projects/
+
+# cd to /Development/Python/banksync_Package
+# it is important the script is run from there.
+
+pip3 uninstall banksync
+python3 setup.py sdist bdist_wheel
+
+#python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# Enter the TestPyPI user: jasonfh
+# Enter the TestPyPI pass: **************
+
+# The following uses the information in ~/.pypirc
+python3 -m twine upload --repository testpypi dist/*
+
+
+sleep 5
+
+# install the package locally from TestPyPI:
+pip3 install -i https://test.pypi.org/simple/ banksync
